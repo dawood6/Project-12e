2c574789a0a19a7281182938f236e53d52afad9a
dawood6 <dawoodsadiq171@gmail.com>
2020-12-18 21:41:56 +0500

    feat: 12e

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-typescript",
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "GCMS",
-        fieldName: "get_lollies",
-        url: "https://virtualLolly-dawood/.netlify/functions/virtalLolipop",
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-graphql",
+    //   options: {
+    //     typeName: "GCMS",
+    //     fieldName: "get_lollies",
+    //     url: "https://virtualLolly-dawood/.netlify/functions/virtalLolipop",
+    //   },
+    // },
   ],
 }; //virtualLolly-dawood
