dab2c032eee13e7acc2e53f7592bf29f3bfb666c
dawood6 <dawoodsadiq171@gmail.com>
2020-12-18 23:49:06 +0500

    feat: 12e

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
-// const path = require("path");
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions;
+const path = require("path");
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
 
-//   const { data } = await graphql(`
-//     {
-//       get_lollies {
-//         getLollies {
-//           colorOne
-//           colorTwo
-//           colorThree
-//           link
-//           sender
-//           reciever
-//           message
-//         }
-//       }
-//     }
-//   `);
+  const { data } = await graphql(`
+    {
+      get_lollies {
+        getLollies {
+          colorOne
+          colorTwo
+          colorThree
+          link
+          sender
+          reciever
+          message
+        }
+      }
+    }
+  `);
 
-//   data.get_lollies.getLollies.forEach((node) => {
-//     createPage({
-//       path: `lolly/${node.link}`,
-//       component: path.resolve("./src/template/template.tsx"),
-//       context: {
-//         colorOne: node.colorOne,
-//         colorTwo: node.colorTwo,
-//         colorThree: node.colorThree,
-//         link: node.link,
-//         message: node.message,
-//         sender: node.sender,
-//         reciever: node.reciever,
-//       },
-//     });
-//   });
-// };
+  data.get_lollies.getLollies.forEach((node) => {
+    createPage({
+      path: `lolly/${node.link}`,
+      component: path.resolve("./src/template/template.tsx"),
+      context: {
+        colorOne: node.colorOne,
+        colorTwo: node.colorTwo,
+        colorThree: node.colorThree,
+        link: node.link,
+        message: node.message,
+        sender: node.sender,
+        reciever: node.reciever,
+      },
+    });
+  });
+};
