36265d088905f3579029b36ed7f12dcb666f7963
dawood6 <dawoodsadiq171@gmail.com>
2020-12-19 00:03:29 +0500

    feat: 12e

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -28,7 +28,25 @@ const codeStyles = {
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-  hello
+      <title>Not found</title>
+      <h1 style={headingStyles}>Page not found</h1>
+      <p style={paragraphStyles}>
+        Sorry{" "}
+        <span role="img" aria-label="Pensive emoji">
+          😔
+        </span>{" "}
+        we couldn’t find what you were looking for. 
+        <br/>
+        {process.env.NODE_ENV === "development" ? (
+          <>
+            <br/>
+              Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            <br/>
+          </>
+        ) : null}
+      <br/>
+        <Link to="/">Go home</Link>.
+      </p>
     </main>
   )
 }
