9b349a06d773c99f95ab10965d5aaf1f56a0ff71
dawood6 <dawoodsadiq171@gmail.com>
2020-12-19 14:42:54 +0500

    feat: 12e

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
-const path = require("path");
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
+// const path = require("path");
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
 
-  const { data } = await graphql(`
-    {
-      get_lollies {
-        getLollies {
-          colorOne
-          colorTwo
-          colorThree
-          link
-          sender
-          reciever
-          message
-        }
-      }
-    }
-  `);
+//   const { data } = await graphql(`
+//     {
+//       get_lollies {
+//         getLollies {
+//           colorOne
+//           colorTwo
+//           colorThree
+//           link
+//           sender
+//           reciever
+//           message
+//         }
+//       }
+//     }
+//   `);
 
-  data.get_lollies.getLollies.forEach((node) => {
-    createPage({
-      path: `lolly/${node.link}`,
-      component: path.resolve("./src/template/template.tsx"),
-      context: node
-    });
-  });
-};
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions;
+//   data.get_lollies.getLollies.forEach((node) => {
+//     createPage({
+//       path: `lolly/${node.link}`,
+//       component: path.resolve("./src/template/template.tsx"),
+//       context: node
+//     });
+//   });
+// };
+// exports.onCreatePage = async ({ page, actions }) => {
+//   const { createPage } = actions;
 
-  if (page.path.match(/^\/lolly/)) {
-    page.matchPath = "/lolly/*";
-    createPage(page);
-  }
-};
+//   if (page.path.match(/^\/lolly/)) {
+//     page.matchPath = "/lolly/*";
+//     createPage(page);
+//   }
+// };
